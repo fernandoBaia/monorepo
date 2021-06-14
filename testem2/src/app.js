f94cb19c8516c2474008e3380e06db86b296d865
Fernando Nascimento <fernandonascimento1502@gmail.com>
2021-06-14 16:34:50 -0300

    fix

---
+++ testem2/src/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const responseTime = require('response-time')
+const app = express()
+const port = 3333
+
+app.use(responseTime())
+
+app.get('/', async (req, res) => {
+    res.send('service 2')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
