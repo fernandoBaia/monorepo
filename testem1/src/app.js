5ca13efc3b6718751d5adac52349bbd9f6f75cfd
Fernando Nascimento <fernandonascimento1502@gmail.com>
2021-06-17 15:19:17 -0300

    rel

--- testem1/src/app.js
+++ testem1/src/app.js
@@ -6,7 +6,7 @@ const port = 3333
 app.use(responseTime())
 
 app.get('/', async (req, res) => {
-    res.send('service 1 in release')
+    res.send('service 1 in release -')
 })
 
 app.listen(port, () => {
