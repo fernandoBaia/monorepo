839fd7a6194229a334c5a8b6bae354047cce1993
Fernando Nascimento <fernandonascimento1502@gmail.com>
2021-09-09 23:11:12 -0300

    teste

--- testem1/src/app.js
+++ testem1/src/app.js
@@ -1,31 +1,32 @@
-const express = require('express')
-const responseTime = require('response-time')
+const express = require('express');
+const responseTime = require('response-time');
 const appInsights = require('applicationinsights');
-const app = express()
-const port = 3333
+const logger = require('../logger');
+const app = express();
+const port = 3333;
 
-app.use(responseTime())
+app.use(responseTime());
 
-appInsights.setup("6a2919f2-be99-43a9-bbe9-088bd46b22ba")
-    .setAutoDependencyCorrelation(true)
-    .setAutoCollectRequests(true)
-    .setAutoCollectPerformance(true, true)
-    .setAutoCollectExceptions(true)
-    .setAutoCollectDependencies(true)
-    .setAutoCollectConsole(true, true)
-    .setUseDiskRetryCaching(true)
-    .setSendLiveMetrics(false)
-    .setDistributedTracingMode(appInsights.DistributedTracingModes.AI_AND_W3C)
-    .start();
+appInsights
+  .setup('6a2919f2-be99-43a9-bbe9-088bd46b22ba')
+  .setAutoDependencyCorrelation(true)
+  .setAutoCollectRequests(true)
+  .setAutoCollectPerformance(true, true)
+  .setAutoCollectExceptions(true)
+  .setAutoCollectDependencies(true)
+  .setAutoCollectConsole(true, true)
+  .setUseDiskRetryCaching(true)
+  .setSendLiveMetrics(false)
+  .setDistributedTracingMode(appInsights.DistributedTracingModes.AI_AND_W3C)
+  .start();
 
 app.get('/', async (req, res) => {
-    console.log('service 1')
-    res.send('service 1 - with error')
-})
-//InstrumentationKey=6a2919f2-be99-43a9-bbe9-088bd46b22ba;
-//IngestionEndpoint=https://brazilsouth-0.in.applicationinsights.azure.com/
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  logger.info('info log');
+  logger.warn('warn log');
+  logger.error('error log');
+  res.send('service 1');
+});
 
-//npm i applicationinsights
+app.listen(port, () => {
+  logger.info('start testem1 on 3333');
+});
