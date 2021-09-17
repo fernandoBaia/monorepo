11bd8c35f7053a6eaa1b279fa834933a8ee7e2d0
Fernando Nascimento <fernandonascimento1502@gmail.com>
2021-09-17 15:04:48 -0300

    run test

--- testem1/src/app.js
+++ testem1/src/app.js
@@ -24,7 +24,7 @@ app.get('/', async (req, res) => {
   logger.info('info log');
   logger.warn('warn log');
   logger.error('error log');
-  res.send('service 1');
+  res.send('service 1 teste');
 });
 
 app.listen(port, () => {
